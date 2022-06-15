f59430e18b75391f406a8a840d60bf009d09aa0b
suyog1601 <89102094+suyog1601@users.noreply.github.com>
2022-06-15 13:47:54 +0530

    Add files via upload

---
+++ ScoreboardAutomation/rmageddon.ino
@@ -0,0 +1,85 @@
+
+
+
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+
+// Replace with your network credentials
+const char* ssid = "suyoglaptop";
+const char* password = "suyogmoon";
+
+// Set LED GPIO
+const int points = 2;
+// Stores LED state
+String pointstatus;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Replaces placeholder with LED state value
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if(digitalRead(ledPin)){
+      pointstatus = "+10";
+    }
+    else{
+      pointstatus = "-10";
+    }
+    Serial.print(pointstatus);
+    return pointstatus;
+  }
+  return String();
+}
+ 
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  pinMode(points, OUTPUT);
+
+  // Initialize SPIFFS
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  // Route to set GPIO to HIGH
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(points, HIGH);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to set GPIO to LOW
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(points, LOW);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+  // Start server
+  server.begin();
+}
+ 
+void loop(){
+  
+}
